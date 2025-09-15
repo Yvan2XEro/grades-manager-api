fcf9042a5de652f10754276105b85b0d6e8c05cb
Yvan2xEro <yvankanaj@gmail.com>
2025-09-15 07:57:25 +0100

    fix: search ui fix

--- apps/web/src/pages/admin/UserManagement.tsx
+++ apps/web/src/pages/admin/UserManagement.tsx
@@ -241,7 +241,7 @@ export default function UserManagement() {
 
 			<div className="mb-4">
 				<div className="relative">
-					<Search className="absolute top-3 left-3 h-4 w-4 text-gray-400" />
+					<Search className="absolute top-3 left-3 h-5 w-5 text-gray-400" />
 					<input
 						type="text"
 						value={search}
