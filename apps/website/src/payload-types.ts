b71cc3cb373acbe7f364ca356ffbd4ddbf500e98
Yvan2xEro <yvankanaj@gmail.com>
2026-06-19 13:41:29 +0100

    fix: payload cms types

--- apps/website/src/payload-types.ts
+++ apps/website/src/payload-types.ts
@@ -458,6 +458,8 @@ export interface User {
 	resetPasswordExpiration?: string | null;
 	salt?: string | null;
 	hash?: string | null;
+	_verified?: boolean | null;
+	_verificationToken?: string | null;
 	loginAttempts?: number | null;
 	lockUntil?: string | null;
 	sessions?:
@@ -892,6 +894,9 @@ export interface InstanceEvent {
 	id: string;
 	instance: string | InstanceRequest;
 	eventType:
+		| "submitted"
+		| "approved"
+		| "rejected"
 		| "provisioned"
 		| "restarted"
 		| "stopped"
@@ -1596,6 +1601,8 @@ export interface UsersSelect<T extends boolean = true> {
 	resetPasswordExpiration?: T;
 	salt?: T;
 	hash?: T;
+	_verified?: T;
+	_verificationToken?: T;
 	loginAttempts?: T;
 	lockUntil?: T;
 	sessions?:
